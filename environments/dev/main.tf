e537ac3314b50a310b44582cf8ca07a04e5c066d
Abhilash-Chincholkar <abhilashchincholkar@gmail.com>
2025-09-29 23:19:58 +0530

    another rg and stage

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -3,3 +3,9 @@ module "resource_group" {
     name = "mnt-rg"
     location = "West India"
 }
+
+module "resource_group_1" {
+    source = "../../modules/azurerm_resource_group"
+    name = "mnt-rg-1"
+    location = "West India"
+}
